6fbd44032b9ccdbed6d8061c82f8b32ff1331178
Ahmed Memic <ahmedmemic@live.com>
2024-04-21 12:00:10 +0200

    add from

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -6,6 +6,8 @@ use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -25,5 +27,35 @@ class MicroPostController extends AbstractController
         return $this->render('micro_post/show.html.twig', [
             'post' => $post
         ]);
+    }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(Request $request, MicroPostRepository $posts): Response
+    {
+        $microPost = new MicroPost();
+        $form = $this->createFormBuilder($microPost)
+            ->add('title')
+            ->add('text')
+            ->add('submite', SubmitType::class, ['label' => 'Save'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->add($post, true);
+
+            $this->addFlash('success', 'Your micro post has been added');
+
+            return $this->redirectToRoute('app_micro_post');
+        }
+
+        return $this->render(
+            'micro_post/add.html.twig',
+            [
+                'form' => $form
+            ]
+        );
     }
 }
