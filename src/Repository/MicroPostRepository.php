17a54c6244f83e1afd23917232403f7ff378cc49
Ahmed Memic <ahmedmemic@live.com>
2024-05-09 19:14:00 +0200

    add query for post comments

--- src/Repository/MicroPostRepository.php
+++ src/Repository/MicroPostRepository.php
@@ -40,6 +40,17 @@ class MicroPostRepository extends ServiceEntityRepository
         }
     }
 
+    public function findAllByComments(): array
+    {
+        return $this->createQueryBuilder('p')
+            ->addSelect('c')
+            ->leftJoin('p.comments', 'c')
+            ->orderBy('p.created', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
     //    /**
     //     * @return MicroPost[] Returns an array of MicroPost objects
     //     */
